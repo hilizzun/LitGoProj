d96c17b85b376815032c01ece6f7f133845fd618
Ascad3 <vd.uv@yandex.ru>
2026-04-26 23:50:56 +0300

    Initialize MapKit in Application and wire manifest

---
+++ app/src/main/java/com/example/myapplication/LitGoApplication.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication
+
+import android.app.Application
+import com.yandex.mapkit.MapKitFactory
+
+class LitGoApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.MAPKIT_API_KEY.isNotBlank()) {
+            MapKitFactory.setApiKey(BuildConfig.MAPKIT_API_KEY)
+            MapKitFactory.initialize(this)
+        }
+    }
+}
